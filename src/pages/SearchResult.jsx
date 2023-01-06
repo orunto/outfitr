cb18965a6882bb1da285062615160defc4cbe0eb
Orunto Eniola <oruntoeniola@aol.com>
2023-01-06 16:37:52 +0100

    feat: Random Gen Page link

--- src/pages/SearchResult.jsx
+++ src/pages/SearchResult.jsx
@@ -1,6 +1,22 @@
+import Copyright from '../images/copyright.svg'
+import Logo from '../images/outfitr-logo.svg'
+import Search from '../images/Search.svg'
+import '../css/result.css'
+// import {RandomGenImage, SearchBar} from '../components'
+
 const SearchResult = () =>{
     return(
-        <div></div>
+        <div className="Result">
+            <div className='ResultContent'>
+                <div className="Header">
+                    <img className='Logo' src={Logo} />
+                    <img className='Search' src={Search} />
+                </div>
+            </div>
+
+            <span><img src={Copyright} alt='copyright icon'/>Softdrobe Inc. 2023</span>
+
+        </div>
     )
 }
 
