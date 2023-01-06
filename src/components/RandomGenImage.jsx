220474d32248449041add45bb338d3f3b2e368ba
Orunto Eniola <oruntoeniola@aol.com>
2023-01-07 00:08:16 +0100

    feat: Random Image Genereator 1.0

--- src/components/RandomGenImage.jsx
+++ src/components/RandomGenImage.jsx
@@ -1,6 +1,6 @@
 const RandomGenImage = (props) => {
     return(
-        <div className="RandomGenImage" id={props.id}>
+        <div className="RandomGenImage" style={props.style} id={props.id}>
             <button>See more like this</button>
         </div>
     )
