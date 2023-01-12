0894e5f94437d609541f032317b3a04cab808bbd
Orunto Eniola <oruntoeniola@aol.com>
2023-01-12 16:53:36 +0100

    fix: Preloader with the appropriate animation

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Home, RandomGen, SearchResult } from './pages';
+import { useState } from 'react';
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const spinner = document.getElementById("spinner");
+  if (spinner) {
+    setTimeout(() => {
+      spinner.style.display = "none";
+      setLoading(false);
+    }, 2000);
+  }
+
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/search' element={<SearchResult/>}/>
-        <Route path='/random' element={<RandomGen/>}/>
-        <Route path='*' element={<Home/>}/>
-      </Routes>
-    </Router>    
+    !loading && (
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/search' element={<SearchResult/>}/>
+          <Route path='/random' element={<RandomGen/>}/>
+          <Route path='*' element={<Home/>}/>
+        </Routes>
+      </Router>    
+    )
   );
 }
 
